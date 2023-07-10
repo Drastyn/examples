288b53f43aed12d1028800a96a02a589a98ac2de
Drastyn <44484749+Drastyn@users.noreply.github.com>
2023-07-10 15:33:35 -0400

    Add | substraction methods (#1)

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const main = () => {
-    let firstSum  = sumNumbers(1, 2)
-    let secondSum = sumNumbers(4, 6)
+    let firstSum        = sumNumbers(1, 2)
+    let secondSum       = sumNumbers(4, 6)
+    let firstSubstract  = substractNumbers(10, 3)
+    let secondSubstract = substractNumbers(9, 4)
     console.log(firstSum)
     console.log(secondSum)
+    console.log(firstSubstract)
+    console.log(secondSubstract)
 }
 
 const sumNumbers = (first, second) => {
     return first + second
+}
+
+const substractNumbers = (first, second) => {
+    return first - second
 }
 
 main()
