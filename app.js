08454cf7b20c4da36ee3fb98248f47127ea4e571
Drastyn <joaquin.dinamarcad@utem.cl>
2023-07-10 15:38:26 -0400

    Change | test to assing new value to constant

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
+const MAX_NUMER = 9
+
 const main = () => {
-    let firstSum        = sumNumbers(1, 2)
-    let secondSum       = sumNumbers(4, 6)
-    let firstSubstract  = substractNumbers(10, 3)
-    let secondSubstract = substractNumbers(9, 4)
-    console.log(firstSum)
-    console.log(secondSum)
-    console.log(firstSubstract)
-    console.log(secondSubstract)
-}
-
-const sumNumbers = (first, second) => {
-    return first + second
-}
-
-const substractNumbers = (first, second) => {
-    return first - second
+    if(MAX_NUMER > 2) MAX_NUMER = 20
 }
 
 main()
