490d8ac4b351a3b6067785b18213649cb2ff48b1
Drastyn <44484749+Drastyn@users.noreply.github.com>
2023-07-11 00:02:17 +0000

    Change - code

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
+const MAX_NUMBER = 4
+
 const main = () => {
-    let firstSum        = sumNumbers(1, 2)
-    let secondSum       = sumNumbers(4, 6)
-    let firstSubstract  = substractNumbers(10, 3)
-    let secondSubstract = substractNumbers(9, 4)
-    console.log(firstSum)
-    console.log(secondSum)
-    console.log(firstSubstract)
-    console.log(secondSubstract)
-}
-
-const sumNumbers = (first, second) => {
-    return first + second
-}
-
-const substractNumbers = (first, second) => {
-    return first - second
+    if (MAX_NUMBER > 2) MAX_NUMBER = 9
 }
 
 main()
